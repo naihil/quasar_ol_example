57a26cd708aed2f1f6e268ea700e918ee1f353b1
Anatoliy Golubev <darth.naihil@gmail.com>
2024-01-18 19:13:38 +0300

    init

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <ol-map
+      :loadTilesWhileAnimating="true"
+      :loadTilesWhileInteracting="true"
+      style="height: 500px; width: 500px"
+    >
+      <ol-view
+        ref="view"
+        :center="center"
+        :rotation="rotation"
+        :zoom="zoom"
+        :projection="projection"
+      />
+
+      <ol-tile-layer>
+        <ol-source-osm />
+      </ol-tile-layer>
+    </ol-map>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+const center = ref([40, 40]);
+const projection = ref("EPSG:4326");
+const zoom = ref(8);
+const rotation = ref(0);
+</script>
